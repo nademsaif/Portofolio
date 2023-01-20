5d4343a8999c07f118d287ce2261a171292b749b
naee44 <nademsaif@gmail.com>
2023-01-20 21:02:25 +0200

    favicon added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,15 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Head from 'next/head'
 
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>Nadeem Saif</title>
+        <link rel="icon" type="image/svg+xml" href="/briefcase.ico" />
+      </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
